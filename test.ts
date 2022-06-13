ca1b3231e49a52bb8cb54e4baea17746147a5b62
healthywalk <89001945+healthywalk@users.noreply.github.com>
2022-06-14 00:10:57 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
 // テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+NiftyMotorDriver2.initializeMotorDriver(MotorEnum.m1, DigitalPin.P8, DigitalPin.P12)
+basic.forever(function () {
+    NiftyMotorDriver2.driveMotor(MotorEnum.m1, 50)
+    basic.pause(2000)
+    NiftyMotorDriver2.coastMotor(MotorEnum.m1)
+    basic.pause(2000)
+    NiftyMotorDriver2.driveMotor(MotorEnum.m1, -50)
+    basic.pause(2000)
+    NiftyMotorDriver2.brakeMotor(MotorEnum.m1)
+    basic.pause(2000)
+})
+
